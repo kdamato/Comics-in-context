97d472b3a674668774471292398cbface1f17994
Kyle DAmato <kyledamato4@gmail.com>
2022-08-09 15:16:09 -0400

    finished comics in context

---
+++ src/RequestFormButton.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext"
+
+function RequestFormButton() {
+  const buttonStyling = React.useContext(StylesContext)
+
+  return (
+    <div>
+      <input style={buttonStyling} type="submit" value="Submit" />
+    </div>
+  );
+}
+
+export default RequestFormButton;
